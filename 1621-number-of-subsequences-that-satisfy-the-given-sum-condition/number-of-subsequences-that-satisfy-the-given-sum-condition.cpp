6008a05c1633b85d836f44b52106d12402a312fa
Rohit Kumar Giri <154068821+Rohit-048@users.noreply.github.com>
2025-06-29 21:37:56 +0530

    Time: 35 ms (38.25%) | Memory: 54.4 MB (34.63%) - LeetSync

--- 1621-number-of-subsequences-that-satisfy-the-given-sum-condition/number-of-subsequences-that-satisfy-the-given-sum-condition.cpp
+++ 1621-number-of-subsequences-that-satisfy-the-given-sum-condition/number-of-subsequences-that-satisfy-the-given-sum-condition.cpp
@@ -1,27 +1,14 @@
 class Solution {
 public:
     int numSubseq(vector<int>& nums, int target) {
-        int mod=1e9+7;
-        int n=nums.size();
-        sort(nums.begin(), nums.end());
-        vector<int>vt(n);
-        vt[0]=1;
-
-        for(int i=1; i<n ; i++){
-            vt[i]=(vt[i-1]*2 )%mod;
+        int n = nums.size(), count = 0, left = 0, right = n-1, mod = 1e9+7;
+        ranges::sort(nums);
+        vector<int> pows(n, 1);
+        for(int i = 1; i < n; i++) pows[i] = pows[i-1] * 2 % mod;
+        while(left <= right) {
+            if(nums[left] + nums[right] > target) right--;
+            else count = (count + pows[right - left]) % mod, left++;
         }
-        int left=0; 
-        int right=n-1;
-        int result=0;
-
-        while(left<=right){
-            if(nums[left]+nums[right]<=target){
-               result=(result+vt[right-left])%mod;
-               left++;
-            }else{
-                right--;
-            }
-        }
-      return result;
+        return count;
     }
 };
