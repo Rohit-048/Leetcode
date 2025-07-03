1c15c44ebe09cdc98b94a9b42f9a3b9a0ce88986
Rohit Kumar Giri <154068821+Rohit-048@users.noreply.github.com>
2025-07-03 11:46:33 +0530

    Time: 121 ms (48.48%) | Memory: 26.2 MB (31.94%) - LeetSync

--- 1890-sum-of-beauty-of-all-substrings/sum-of-beauty-of-all-substrings.cpp
+++ 1890-sum-of-beauty-of-all-substrings/sum-of-beauty-of-all-substrings.cpp
@@ -1,25 +1,23 @@
 class Solution {
 public:
     int beautySum(string s) {
-        int n=s.length();
-        int sum=0;
-        for(int i=0;i<n;i++){
-            unordered_map<char,int> freqMap;
-            for(int j=i;j<n;j++){
-                freqMap[s[j]]++;
+        int n = s.length();
+        int sum = 0;
 
-                int minVal=INT_MAX;
-                int maxVal=INT_MIN;
+        for (int i = 0; i < n; i++) {
+            unordered_map<int, int> mp;
+            for (int j = i; j < n; j++) {
+                mp[s[j]]++;
 
-                for(auto &p: freqMap){
-                    minVal=min(minVal,p.second);
-                    maxVal=max(maxVal,p.second);
+                int minVal = INT_MAX;
+                int maxVal = INT_MIN;
+                for (auto& it : mp) {
+                    minVal = min(minVal, it.second);
+                    maxVal = max(maxVal, it.second);
                 }
-                sum+=(maxVal-minVal);
+                sum += (maxVal - minVal);
             }
-       
         }
         return sum;
-
     }
 };
