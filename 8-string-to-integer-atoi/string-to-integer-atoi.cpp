214c94e8e3935e9ae575aae20e5fe0a718f65b56
Rohit Kumar Giri <154068821+Rohit-048@users.noreply.github.com>
2025-07-03 11:11:58 +0530

    Time: 0 ms (100.00%) | Memory: 9 MB (81.72%) - LeetSync

--- 8-string-to-integer-atoi/string-to-integer-atoi.cpp
+++ 8-string-to-integer-atoi/string-to-integer-atoi.cpp
@@ -1,32 +1,25 @@
 class Solution {
 public:
     int myAtoi(string s) {
-
+        int n = s.size();
         int i = 0;
-        int sign = 1; // positive by default
-        int n = s.length();
+        long long result = 0;
+        int sign = 1;
 
-        // to check for leading whitespaces
         while (i < n && isspace(s[i])) {
             i++;
         }
-        // to check for any sign positive or negative
-        if (i < n && (s[i] == '-' || s[i] == '+')) {
-            sign = (s[i++] == '+') ? 1 : -1;
+        if ( i<n && (s[i] == '-' || s[i] == '+')) {
+            sign = (s[i] == '-') ? -1 : 1;
+            i++;
         }
-
-        // finding the actual result in digits
-        long long result = 0;
         while (i < n && isdigit(s[i])) {
             result = result * 10 + (s[i++] - '0');
 
-            if (result >
-                INT_MAX) { // if it is more then int max so it will also be less
-                           // then int min so just return that value first
-                return sign == -1 ? INT_MIN : INT_MAX;
+            if (result > INT_MAX) {
+                return sign == 1 ? INT_MAX : INT_MIN;
             }
         }
-
         return result * sign;
     }
 };
